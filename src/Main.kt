57ac4ab4fbeb78900d1bf2baf8ff2e83eafff4e1
Ajgiz_Mason <ajgizmavletberdin@gmail.com>
2025-06-06 16:13:36 +0500

    Revert "1"
    This reverts commit 04f78aa6e18f1249f13ac235d17b0b88684cfe9a.

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,5 @@
-fun main(){
-    var Wolf = Stalker(NameOrNickname = "Wolf", Rank = "Veteran", Location = "Newcomer's vilage", Group = "Stalker")
-    Wolf.SwitchRank("Master")
-    Wolf.SwitchLocation("Army Depots")
-    Wolf.AddEquipment("Ak-74M")
-    Wolf.AddEquipment("\"Soul\"")
-    Wolf.AddEquipment("F-1")
-    Wolf.SwitchGroup("Bandit")
-    println("Ранг сталкера Wolf: ${Wolf.Rank} \nЕго локация: ${Wolf.Location}\nЕго Группировка: ${Wolf.Group} \nЕго снаряга: ${Wolf.Equipment}" )
+fun main() {
+
 
 
 }
